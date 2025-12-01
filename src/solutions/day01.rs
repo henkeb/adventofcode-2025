093106602d0634e8a3e2ffeecad4185612ae7f00
Henrik Berg <khp.berg@gmail.com>
2025-12-01 09:19:35 +0100

    Clean-up

--- src/solutions/day01.rs
+++ src/solutions/day01.rs
@@ -1,69 +1,76 @@
 pub fn puzzle_1(input: &str) -> String {
-    let mut dial = 50;
-    let mut password = 0;
-    for line in input.lines() {
-        let (direction, rotation) = line.split_at(1);
-        let mut rotation = rotation.parse::<u32>().unwrap();
-
-        rotation = rotation % 100;
-
-        match direction {
-            "L" => {
-                if rotation > dial {
-                    dial = 100 - rotation + dial;
-                } else {
-                    dial -= rotation;
+    input
+        .lines()
+        .fold((0, 50), |(mut password, mut dial), line| {
+            match (line.chars().nth(0), line[1..].parse::<u32>()) {
+                (Some(direction), Ok(mut rotation)) => {
+                    rotation %= 100;
+                    match direction {
+                        'L' => {
+                            if rotation > dial {
+                                dial = 100 - rotation + dial;
+                            } else {
+                                dial -= rotation;
+                            }
+                        }
+                        'R' => {
+                            dial += rotation;
+                        }
+                        _ => panic!("Wrong input: {}", direction),
+                    }
                 }
+                _ => panic!("Wrong input"),
             }
-            "R" => {
-                dial = (dial + rotation) % 100;
+
+            dial %= 100;
+            if dial == 0 {
+                password += 1;
             }
-            _ => panic!("Wrong input"),
-        }
-        dial %= 100;
-        if dial == 0 {
-            password += 1;
-        }
-    }
-    password.to_string()
+            (password, dial)
+        })
+        .0
+        .to_string()
 }
 
 pub fn puzzle_2(input: &str) -> String {
-    let mut dial = 50;
-    let mut password = 0;
-    for line in input.lines() {
-        let (direction, rotation) = line.split_at(1);
-        let mut rotation = rotation.parse::<u32>().unwrap();
-
-        password += rotation / 100;
-
-        rotation = rotation % 100;
-
-        match direction {
-            "L" => {
-                if rotation > dial {
-                    if dial != 0 {
-                        password += 1;
+    input
+        .lines()
+        .fold((0, 50), |(mut password, mut dial), line| {
+            match (line.chars().nth(0), line[1..].parse::<u32>()) {
+                (Some(direction), Ok(mut rotation)) => {
+                    password += rotation / 100;
+                    rotation %= 100;
+                    match direction {
+                        'L' => {
+                            if rotation > dial {
+                                if dial != 0 {
+                                    password += 1;
+                                }
+                                dial = 100 - rotation + dial;
+                            } else {
+                                dial -= rotation;
+                            }
+                        }
+                        'R' => {
+                            dial += rotation;
+                            if dial > 100 {
+                                password += 1;
+                            }
+                        }
+                        _ => panic!("Wrong input: {}", direction),
                     }
-                    dial = 100 - (rotation - dial);
-                } else {
-                    dial -= rotation;
                 }
+                _ => panic!("Wrong input"),
             }
-            "R" => {
-                dial += rotation;
-                if dial > 100 {
-                    password += 1;
-                }
+
+            dial %= 100;
+            if dial == 0 {
+                password += 1;
             }
-            _ => panic!("Wrong input"),
-        }
-        dial %= 100;
-        if dial == 0 {
-            password += 1;
-        }
-    }
-    password.to_string()
+            (password, dial)
+        })
+        .0
+        .to_string()
 }
 
 #[cfg(test)]
